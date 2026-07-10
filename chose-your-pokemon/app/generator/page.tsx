b76a9fb10b2eeabd7fe4e1cc044f3817ce6cb041
Azra Ali <azra_ali09@outlook.com>
2026-07-10 16:34:33 -0400

    api stuff works!! just working on designing the website

--- chose-your-pokemon/app/generator/page.tsx
+++ chose-your-pokemon/app/generator/page.tsx
@@ -12,20 +12,20 @@ export default function App(){
     const[loading, setLoading] = useState(false);
 
     const generateImage = async () => {
-      setLoading(true);
+    setLoading(true);
         try{
             const random = Math.floor(Math.random()*1025)+1;
             const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${random}`);
             const pokemon = await response.json();
             //used ai to help debug this part
             const names = pokemon.name;
-            const images = pokemon.sprites.front_shiny;
+            const images = pokemon.sprites.front_default;
             setName(names);
             setImage(images);
         } catch (error){
             console.error('Error fetching pokemon:', error);
         } finally{
-          setLoading(false);
+        setLoading(false);
         }
     };
     //referenced ai for this line
@@ -33,17 +33,22 @@ export default function App(){
         generateImage();
     }, []);
     return(
-        <div>
-        <h1>Your Pokemon is!</h1>
-        <div>
-            <h2 className="font-pokemon">{name}</h2>
-            <Image
+        <div className="flex flex-col text-center items-center min-h-screen justify-center bg-cyan-50">
+        <h1 className="pt-20 font-pokemon text-black text-8xl">Your Pokemon is:</h1>
+        <div className="pt-10 items-center flex-col flex ">
+            <h2 className="pt-20 font-pokemon text-black text-7xl">{name}!</h2>
+            <img
+            className="w-150 -mt-8 h-150 items-center hover:animate-bounce"
             src={image}
             alt={name}
-            width={100}
-            height={100}
-            ></Image>
+            />
         </div>
+        <a
+            className="items-center justify-center w-10 flex h-12 gap-3 rounded-full px-15 text-background bg-sky-200 hover:bg-sky-100"
+            href="/"
+        >
+        back!
+        </a>
         </div>
     );
 
