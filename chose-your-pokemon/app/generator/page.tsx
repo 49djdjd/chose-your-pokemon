bf818c35f46bce63af89ba398b4d10b603ee444b
Azra Ali <azra_ali09@outlook.com>
2026-07-10 16:17:22 -0400

    working on the generator page, running into issues with deciding on how to do the button

--- chose-your-pokemon/app/generator/page.tsx
+++ chose-your-pokemon/app/generator/page.tsx
@@ -1,10 +1,48 @@
+'use client';
 import Link from "next/link";
+
 import Image from "next/image";
 
-export default function Home(){
+import { useState, useEffect } from "react";
+
+export default function App(){
+    const[image, setImage] = useState('');
+    const[name, setName] = useState('');
+    //used ai to connect the button from home page
+    const[loading, setLoading] = useState(false);
+
+    const generateImage = async () => {
+      setLoading(true);
+        try{
+            const random = Math.floor(Math.random()*1025)+1;
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${random}`);
+            const pokemon = await response.json();
+            //used ai to help debug this part
+            const names = pokemon.name;
+            const images = pokemon.sprites.front_shiny;
+            setName(names);
+            setImage(images);
+        } catch (error){
+            console.error('Error fetching pokemon:', error);
+        } finally{
+          setLoading(false);
+        }
+    };
     return(
-        <div className="flex flex-col flex-1 text-5xl text-black items-center justify-start gap-5 pt-21 font-pokemon bg-cyan-50">
-          <h1>Your Pokemon is</h1>
-    </div>
-  );
+        <div>
+        <h1>Your Pokemon is!</h1>
+        <div>
+            <h2 className="font-pokemon">{name}</h2>
+            <Image
+            className="hover:animate-bounce"
+            src={image}
+            alt={name}
+            ></Image>
+        </div>
+        <button onClick={generateImage}
+        disabled={loading}
+        {loading ? "catching"}> </button>
+        </div>
+    )
+
 }
--- chose-your-pokemon/app/index.tsx
+++ chose-your-pokemon/app/index.tsx
@@ -7,13 +7,21 @@ const App = () => {
     const generateImage = async () => {
         try{
             const random = Math.floor(Math.random()*1025)+1;
-            const response = await fetch('https://pokeapi.co/api/v2/pokemon{random}');
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon/${random}');
             const pokemon = await response.json();
             //used ai to help debug this part
             const names = pokemon.name;
-            const images = pokemon.sprites
-
+            const images = pokemon.sprites.front_shiny;
+            setName(names);
+            setImage(images);
+        } catch (error){
+            console.error('Error fetching pokemon:', error);
         }
-    }
+    };
+    return(
+        <div>
+            <h1>Generator</h1>
+        </div>
+    )
 
 }
