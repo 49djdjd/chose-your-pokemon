13168ae0948b5f649757c1a9c3572e8741455441
Azra Ali <azra_ali09@outlook.com>
2026-07-10 14:29:39 -0400

    added gradient on title text

--- chose-your-pokemon/app/page.tsx
+++ chose-your-pokemon/app/page.tsx
@@ -5,26 +5,27 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-start gap-5 pt-21 font-sans bg-cyan-50">
         <div className="flex flex-grid items-start text-center sm:items-start sm:text-left">
         <Image
-        className="-mt-5"
+        className="-mt-5 hover:animate-bounce"
           src="/meowth.png"
           alt="Next.js logo"
 
-          width={100}
-          height={20}
+          width={150}
+          height={40}
         />
-          <h1 className="text-5xl font-pokemon text-center tracking-tight text-black">
+          <h1 className="text-8xl p-5 bg-clip-text hover:text-transparent hover:bg-linear-to-r from-blue-500 via blue-50 to-amber-300 font-pokemon text-center text-black ">
             Welcome to Pokemon Chooser!
           </h1>
         </div>
-        <div className="flex flex-col items-center gap-4 text-2xl font-medium sm:flex-row">
+        <div className="flex flex-col items-center mt-60 gap-4 text-5xl font-medium sm:flex-row">
         <Image
+            className="hover:animate-bounce"
               src="/piplup.png"
               alt="Vercel logomark"
               width={100}
               height={20}
             />
           <a
-            className="items-center justify-center flex h-12 w-full gap-2 rounded-full bg-foreground px-15 text-background transition-colors hover:bg-[#383838] sm:w-auto"
+            className="items-center justify-center flex h-15 w-full gap-2 rounded-full bg-foreground px-15 text-background transition-colors hover:bg-[#383838] sm:w-auto"
             href="/quiz"
           >
             Start Quiz!
